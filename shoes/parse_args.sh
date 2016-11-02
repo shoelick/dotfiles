0558a9106af2457bff4aaae6e16e8e7a0ee747f9
shoelick <owl@LimbOS.neo.rr.com>
2016-11-01 22:38:01 -0400

    I think argument parsing is complete. Learned that bash doesn't support exporting arrays and zsh doesn't support  (fml). Up next is to figure out this call to detect platform.

--- shoes/parse_args.sh
+++ shoes/parse_args.sh
@@ -9,7 +9,6 @@
 ## AreArgsValid <Passed parameters array>
 # Returns 0 if the passed arguments are valid based on shoesfiles 
 # configuration.
-#
 function AreArgsValid() {
 
     Debug "AreArgsValid Received $@"
@@ -25,15 +24,42 @@ function AreArgsValid() {
         RetVal=$ERROR_CODE_INVALID_ARGS;
     fi
    
-
-    #
-    if [[ -z ${Cmd+"hahahanotset"} ]] && ! containsElement $Target $SUPPORTED_TARGETS ; then
+    # Check for valid target
+    # First condition is Bash's annoying way of saying "if (var is not null)"
+    if [[ ! -z ${Cmd+"fuckme"} ]] && ! containsElement $Target $SUPPORTED_TARGETS ; then
         Debug "$Target is not in $SUPPORTED_TARGETS";
         echo "Invalid target."
         RetVal=$ERROR_CODE_INVALID_ARGS;
     fi
 
     return $RetVal;
+}
+
+#
+#
+#
+#
+DetectPlatform() {
+
+    # Get *nix distro
+    DISTRO_RAW="" # Gets raw os output
+    DISTRO_RAW_LOC=`echo /etc/*-release` 2> /dev/null
+
+    # If DISTRO_RAW_LOC still contains the '*' (didn't use regex) empty DISTRO_RAW_LOC
+    if [[ "$DISTRO_RAW_LOC" == "/etc/*-release" ]]; then
+        DISTRO_RAW_LOC="";
+    fi
+
+    # get contents of uname or lsb-release
+    if [[ "$DISTRO_RAW_LOC" != "" ]]; then
+        DISTRO_RAW=$(cat /etc/*-release 2> /dev/null)
+    else
+        DISTRO_RAW=$(uname)
+    fi
+
+    # Parses out specific distro
+    #export DISTRO=`echo $DISTRO_RAW | perl -lne '/(Ubuntu)|(Debian)|(Darwin)|(Arch)|(Fedora)/gi && print $&' | head -n1`
+
 }
 
 #
--- shoes/util.sh
+++ shoes/util.sh
@@ -12,7 +12,7 @@
 #
 containsElement () {
     local e
-    for e in "${@:2}"; do [[ "$e" == "$1" ]] && return ; done
+    for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
     return 1
 }
 
@@ -23,7 +23,7 @@ containsElement () {
 #
 Debug () {
 
-    if [ ! -z ${DEBUG_OUTPUT+notset} ] ; then 
+    if [ ! -z ${DEBUG_OUTPUT+"shitt"} ] ; then 
         echo "DEBUG: $@"
     fi
 
@@ -38,4 +38,44 @@ PrintUsage() {
     echo "Supported subcommands: $SUPPORTED_CMDS"
     echo "Supported targets: $SUPPORTED_TARGETS"
 
+}
+
+#
+#
+#
+#
+DetectPlatform() {
+
+    echo "Can you see me? $1"
+    RetVal=$ERROR_CODE_NONE
+
+    # Get *nix distro
+    DISTRO_RAW="" # Gets raw os output
+    DISTRO_RAW_LOC=`echo /etc/*-release` 2> /dev/null
+
+    # If DISTRO_RAW_LOC still contains the '*' (didn't use regex) empty DISTRO_RAW_LOC
+    if [[ "$DISTRO_RAW_LOC" == "/etc/*-release" ]]; then
+        DISTRO_RAW_LOC="";
+    fi
+
+    # get contents of uname or lsb-release
+    if [[ "$DISTRO_RAW_LOC" != "" ]]; then
+        DISTRO_RAW=$(cat /etc/*-release 2> /dev/null)
+    else
+        DISTRO_RAW=$(uname)
+    fi
+
+    echo $DISTRO_RAW
+    # Parses out specific distro
+    for OS in $SUPPORTED_OS; do 
+        Debug "Checking for $OS..."
+        PLATFORM=$(echo $DISTRO_RAW | grep -o "$OS")
+        Debug "Platform is $PLATFORM"
+        [[ ! -z ${PLATFORM+"hehehe"} ]] && break;
+    done
+
+    # Report error 
+    [[ ! -z ${PLATFORM+"shnikey!"} ]] && RetVal=$ERROR_CODE_FAILURE 
+
+    return $RetVal
 }
