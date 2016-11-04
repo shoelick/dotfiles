5fbad86b8cbb6e244feeaeedc6321deb2d2f5123
shoelick <owl@LimbOS.neo.rr.com>
2016-11-03 21:39:42 -0400

    Platform detection is good (for real this time?) and command switch created. Testing dotfiles dir movement. Committing before I lose my stuff.

--- shoes/util.sh
+++ shoes/util.sh
@@ -40,9 +40,39 @@ PrintUsage() {
 
 }
 
+## AreArgsValid <Passed parameters array>
+# Returns 0 if the passed arguments are valid based on shoesfiles 
+# configuration.
+function AreArgsValid() {
+
+    Debug "AreArgsValid Received $@"
+
+    SUBCMD=$1
+    TARGET=$2
+    RetVal=$ERROR_CODE_NONE
+
+    # Subcommand is required
+    if ! containsElement $SUBCMD $SUPPORTED_CMDS ; then
+        Debug "$SUBCMD not in $SUPPORTED_CMDS"
+        echo "Invalid subcommand."
+        RetVal=$ERROR_CODE_INVALID_ARGS;
+    fi
+   
+    # Check for valid target
+    # First condition is Bash's annoying way of saying "if (var is not null)"
+    if [[ ! -z ${SUBCMD+"fuckme"} ]] && ! containsElement $TARGET $SUPPORTED_TARGETS ; then
+        Debug "$TARGET is not in $SUPPORTED_TARGETS";
+        echo "Invalid target."
+        RetVal=$ERROR_CODE_INVALID_ARGS;
+    fi
+
+    return $RetVal;
+}
+
 #
-#
-#
+# Sets PLATFORM to be a one-word string defining operating system.
+# @depends SUPPORTED_OS String of space-separated supported 
+#           operating systems.
 #
 DetectPlatform() {
 
@@ -70,13 +100,85 @@ DetectPlatform() {
     for OS in $SUPPORTED_OS; do 
         Debug "Checking for $OS..."
         PLATFORM=$(echo $DISTRO_RAW | grep -o "$OS")
-        Debug "Platform is $PLATFORM"
         [[ ! -z ${PLATFORM+"hehehe"} ]] && break;
     done
 
     # Report error 
-    [[ ! -z ${PLATFORM+"shnikey!"} ]] && RetVal=$ERROR_CODE_FAILURE && \
-        echo "Welp. We don't have your back this time fam. OS not supported." 
+    Debug "Platform is $PLATFORM"
+    [[ -z ${PLATFORM+"shnikey!"} ]] && RetVal=$ERROR_CODE_FAILURE && echo "Welp. We don't have your back this time fam. OS not supported." 
 
     return $RetVal
+}
+
+#
+# Set up and move .dotfiles directory
+# The back up directory does NOT exist by default.
+# Its presence indicates an existing backup.
+# @depends DOTFILES_REPO_DIR Location of the repository
+# @depends DOTFILES_DIR Final destination
+# 
+ConfigureDotfilesDir() {
+
+    RETVAL=$ERROR_CODE_NONE
+    CONTINUE=1
+    BUPDIR=$DOTFILES_REPO_DIR/dotfiles.backup
+
+    # If we are running from final destination, no worries.
+    if [ "$DOTFILES_REPO_DIR" = "$DOTFILES_DIR" ]; then
+        Debug "Already in final destination. Not moving."
+        return $ERROR_CODE_NONE;
+
+    # Otherwise, check if final destination exists
+    elif [ -e "$DOTFILES_DIR" ]; then
+
+        Debug "Dotfiles directory exists and we're not in it."
+        # Check if backup directory exists
+        if [ -e "$BUPDIR" ]; then
+            echo -e "\e[5mWARNING!"
+            PROMPT="$DOTFILES_REPO_DIR/dotfiles.backup exists. Obliterate backup? " 
+            CONTINUE=$(BoolPrompt "$PROMPT")
+        fi
+    fi
+
+    # Exit if user told us to.
+    if [ $CONTINUE = 0 ]; then
+        echo "Empty/delete existing backup and try again." 
+        return $ERROR_CODE_FAIL
+    # Make sure the backup dir doesn't exist
+    else 
+        [ -d "$BUPDIR" ] && rm -v "$BUPDIR"
+    fi
+
+    cd "$HOME"
+    # If the repo is not the final location and the final location exists
+    if  [ -d "$DOTFILES_DIR"  ]; then
+        # Move existing dotfiles dir to a backup dir
+        # A link will not be moved
+        mkdir -pv "$BUPDIR"
+        mv -v "$HOME/.dotfiles" "$DOTFILES_REPO_DIR/dotfiles.backup"
+    fi
+
+    # Move repo to final destination
+    mv -v "$DOTFILES_REPO_DIR" "$DOTFILES_DIR"
+    cd "$HOME/.dotfiles"
+    DOTFILES_BACKUP="$DOTFILES_DIR/dotfiles.backup"
+
+    return $RETVAL
+}
+
+#
+# BooleanPrompt
+# Prompts the user for a yes or no answer
+# @param $1 takes in a string prompt
+# 0 on true or 1 on false
+#
+BoolPrompt() {
+    
+    DONE=0
+    while [ ! $DONE = 1 ]; do
+        read -p "$1 [Y/n]: " INPUT; # prompt
+        INPUT = ${INPUT,,} # make lowercase
+        [[ ($INPUT = "y" || $INPUT = "n") ]] && DONE=1 
+    done;
+    echo [[ $INPUT = "y" ]]
 }
