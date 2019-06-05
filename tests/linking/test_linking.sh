95c751bd631c890d8692b11fa5ee570ff6cd023c
Michael <michael@shullick.io>
2019-06-05 17:14:08 -0400

    Correct make sure prefix exists

--- tests/linking/test_linking.sh
+++ tests/linking/test_linking.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 export PREFIX=$HOME/testhome
+mkdir -p $PREFIX
 
 tests/linking/install.sh.expect
 
-
 cd configs
-for conf_file_path in $(find . -type f | sed "s|^\./||"); do
+for conf_file_path in $(find  -type f | sed "s|^\./||"); do
     if [ ! -L $PREFIX/$conf_file_path ]; then
         echo "No $conf_file_path link found"
         return 1
